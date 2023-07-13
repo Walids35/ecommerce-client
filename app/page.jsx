63905a20b2dbd9a515a88bb4c503a5e94f22a6ce
Walid Siala <wsiala4@gmail.com>
2023-07-13 15:28:44 +0100

    Setting Landing Page Image and Visuals

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import NavBar from "@/components/navbar/NavBar"
-import OverNav from "@/components/navbar/OverNav"
-
 export default function Home() {
   return (
     <>
-      <OverNav />
-      <NavBar />
+      <div className="bg-home-img w-full bg-[right_top] lg:bg-center bg-cover px-10 py-20 sm:p-32" style={{height: "600px"}}>
+        <p className="text-3xl sm:text-5xl text-full-white font-bold">Shopping and <p className="mt-3">Entertainement</p></p>
+        <p className="text-white mt-5 w-80">Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
+        <button className="text-white border-white border-2 mt-10 py-3 px-6 rounded-full">Learn More</button>
+      </div>
     </>
   )
 }
