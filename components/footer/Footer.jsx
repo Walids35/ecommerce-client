7ed130998f6e8609911205f2c53aea42e93f4307
Walid Siala <wsiala4@gmail.com>
2023-07-17 17:47:57 +0100

    Responsive Footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <div className="pb-20 px-10 sm:px-20 md:px-32 xl:px-60">
         <div className="w-full h-0.5 bg-slate-300 rounded-full"></div>
-        <div className="grid p-10 grid-cols-6">
+        <div className="grid p-10 grid-cols-1 gap-10 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6">
           <div className="col-span-2">
             <img src="mini-logo.svg" alt="" className="w-14" />
             <p className="w-72 mt-5 text-sm">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
