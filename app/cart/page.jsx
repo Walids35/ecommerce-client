9aed275101b32ef977ab4f765f84479e97c79271
Walid Siala <wsiala4@gmail.com>
2023-07-31 14:28:33 +0100

    Getting Products From Cart and Looping over

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,9 +1,14 @@
+"use client";
 import Footer from "@/components/footer/Footer";
 import ProductCart from "@/components/product/ProductCart";
 import Link from "next/link";
 import { TruckIcon, GiftIcon } from "@heroicons/react/24/outline";
+import { useStore } from "@/store/store";
 
 const CartComponent = () => {
+  const cart = useStore((state) => state.cartProducts);
+  console.log(cart);
+
   return (
     <>
       <div className="px-10 py-10 sm:px-10 md:px-32 lg:px-60">
@@ -16,10 +21,11 @@ const CartComponent = () => {
               </tr>
               <hr></hr>
               <tr>
-                <th>
-                  <ProductCart />
-                  <ProductCart />
-                </th>
+                {cart.map((product, index) => {
+                  return (
+                    <ProductCart product={product} />
+                  )
+                })}
               </tr>
             </table>
             <div className="mt-8">
@@ -29,40 +35,45 @@ const CartComponent = () => {
             </div>
           </div>
           <div>
-          <table className="mt-5 border w-full border-gray-300 ">
-              <tr>
-                <th className="text-start font-normal p-5">
-                    <div className="flex justify-between">
-                        <p>Articles</p>
-                        <p>200.99£</p>
-                    </div>
-                    <div className="flex justify-between">
-                        <p>Discount</p>
-                        <p>0</p>
-                    </div>
-                    <div className="flex justify-between">
-                        <p>Delivery</p>
-                        <p>7.00£</p>
-                    </div>
-                    <hr className="my-5"></hr>
-                    <div className="flex text-blue font-semibold justify-between">
-                        <p>Total TTC</p>
-                        <p>207.99£</p>
-                    </div>
-                    <hr className="my-5"></hr>
-                    <Link href="/" className="bg-blue text-white mt-10 px-10 py-3">
-                Shop
-              </Link>
-                </th>
-              </tr>
-            </table>
-            <div className="border border-gray-300 flex items-center gap-4 mt-5 rounded-md py-2 px-5 border-l-8 border-l-black">
-                <TruckIcon className="text-black w-8" />
-                <p className="font-medium text-sm">Delivery All Over Tunisia</p>
+            <div className="mt-5 border w-full border-gray-300 ">
+              <div>
+                <div className="text-start font-normal p-5">
+                  <div className="flex justify-between">
+                    <p>Articles</p>
+                    <p>200.99£</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Discount</p>
+                    <p>0</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Delivery</p>
+                    <p>7.00£</p>
+                  </div>
+                  <hr className="my-5"></hr>
+                  <div className="flex text-blue font-semibold justify-between">
+                    <p>Total TTC</p>
+                    <p>207.99£</p>
+                  </div>
+                  <hr className="my-5"></hr>
+                  <Link
+                    href="/"
+                    className="bg-blue text-white mt-10 px-10 py-3"
+                  >
+                    Shop
+                  </Link>
+                </div>
+              </div>
             </div>
             <div className="border border-gray-300 flex items-center gap-4 mt-5 rounded-md py-2 px-5 border-l-8 border-l-black">
-                <GiftIcon className="text-black w-8" />
-                <p className="font-medium text-sm">Free Delivery From 200£ Shopping Cart</p>
+              <TruckIcon className="text-black w-8" />
+              <p className="font-medium text-sm">Delivery All Over Tunisia</p>
+            </div>
+            <div className="border border-gray-300 flex items-center gap-4 mt-5 rounded-md py-2 px-5 border-l-8 border-l-black">
+              <GiftIcon className="text-black w-8" />
+              <p className="font-medium text-sm">
+                Free Delivery From 200£ Shopping Cart
+              </p>
             </div>
           </div>
         </div>
