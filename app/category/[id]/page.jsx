d70febd38b218a123afd7fad2094b76d634c479b
Walid Siala <wsiala4@gmail.com>
2023-08-05 12:26:58 +0100

    Getting parent category and its attributes

--- app/category/[id]/page.jsx
+++ app/category/[id]/page.jsx
@@ -1,57 +1,67 @@
-"use client"
-import { useParams } from 'next/navigation'
-import ProductCard from '@/components/product/ProductCard'
-import Footer from '@/components/footer/Footer'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { mongooseConnect } from '@/lib/mongoose'
+"use client";
+import { useParams } from "next/navigation";
+import ProductCard from "@/components/product/ProductCard";
+import Footer from "@/components/footer/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function page (){
-  const {id} = useParams()
-  const [products, setProducts] = useState([])
+export default function page() {
+  const { id } = useParams();
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState({});
+  const [properties, setProperties] = useState([])
 
-  useEffect(() => (
-    axios.get(`/api/product/${id}`).then((response) => {
-      setProducts(response.data)
-    }).catch((error) => {
+  async function fetchCategory() {
+    try {
+      const response = await axios.get(`/api/category/${id}`);
+      setCategory(response.data[0]);
+    } catch {
       console.log(error);
-    })
-  ),[])
-  const newId = id.replaceAll("-"," ").toUpperCase()
-  
+    }
+  }
+
+  useEffect(
+    () => (
+      axios
+        .get(`/api/product/${id}`)
+        .then((response) => {
+          setProducts(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        }),
+      fetchCategory()
+    ),
+    []
+  );
+
   return (
     <>
-    <div className='px-10 py-10 sm:px-10 md:px-32 lg:px-60'>
-        <h1 className='font-bold text-3xl'>{newId}</h1>
-        <h2 className='font-semibold text-xl'>Today's Best Deals For You !</h2>
-        <div className='mt-5 flex gap-8'>
-            <select className='px-6 py-2 rounded-full bg-blue text-white border-2 border-black'>
-                <option>Sort: Newest First</option>
-                <option>test</option>
-                <option>test</option>
-            </select>
-            <select className='px-6 py-2 rounded-full bg-white text-black border-2 border-black'>
-                <option>Color: All</option>
-                <option>test</option>
-                <option>test</option>
-            </select>
-            <select className='px-6 py-2 rounded-full bg-white text-black border-2 border-black'>
-                <option>Storage: All</option>
-                <option>test</option>
-                <option>test</option>
-            </select>
+      <div className="px-10 py-10 sm:px-10 md:px-32 lg:px-60">
+        <h1 className="font-bold text-3xl">{category && category.name}</h1>
+        <h2 className="font-semibold text-xl">Today's Best Deals For You !</h2>
+        <div className="mt-5 flex gap-8">
+          {category.parent?.properties.map((index, property) => {
+            return(
+              <select name={property}>
+                {category.parent.properties[property].values.map((value) => {
+                  return(<option value={value}>{value}</option>)
+                })}
+              </select>
+            )
+          })}
         </div>
         <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
-        {products.length > 0 ? products.map((product, index) => {
-            return <ProductCard key={index} product={product} />
-        }) : (<div>Waiting ...</div>)}
+          {products.length > 0 ? (
+            products.map((product, index) => {
+              return <ProductCard product={product} />;
+            })
+          ) : (
+            <div>Waiting ...</div>
+          )}
         </div>
-    </div>
-    <Footer />
+      </div>
+      <Footer />
     </>
-  )
-}
-
-export async function getServerSideProps(){
-  await mongooseConnect();
+  );
 }
