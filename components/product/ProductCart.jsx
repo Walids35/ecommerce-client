f5d13b11f72e80037d727fc37110a713e65fe13f
Walid Siala <wsiala4@gmail.com>
2023-08-05 11:20:37 +0100

    Delete Product From Cart by ID

--- components/product/ProductCart.jsx
+++ components/product/ProductCart.jsx
@@ -1,22 +1,21 @@
 import { TrashIcon } from "@heroicons/react/24/solid"
+import { useStore } from "@/store/store"
 
 const ProductCart = ({product}) => {
 
-  const deleteProduct = () => {
-    
-  }
+  const deleteFromCart = useStore((store) => store.removeFromCart);
 
   return (
     <>
-        <div className="flex p-10 justify-around  ">
-            <img src="/headphone.svg" alt="" className="w-16 h-16" />
+        <div className="flex p-10 justify-around" key={product._id}>
+            <img src={product.images[0]} alt="" className="w-16 h-16" />
             <div>
                 <h1>{product.title}</h1>
                 <p className="font-medium text-purple">{product.price}£</p>
                 <p className="font-normal text-sm hidden md:block">[64b5a93cc77e31b306739963]</p>
             </div>
             <div className="text-start font-normal">
-                <p>200.99£</p>
+                <p>{product.price}</p>
                 <div className="flex">
                     <p className="py-2 px-5 bg-gray-200">1</p>
                     <button className="border py-2 px-5 hover:bg-slate-200">+</button>
@@ -24,7 +23,7 @@ const ProductCart = ({product}) => {
                 </div>
             </div>
             <div className="">
-                <button onClick={deleteProduct}>
+                <button onClick={() => deleteFromCart(product._id)}>
                     <TrashIcon className="w-6 hover:text-purple"/>
                 </button>
             </div>
