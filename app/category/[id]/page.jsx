d449ce5588182e0573f55fd15086f81bcc1a4eb1
Walid Siala <wsiala4@gmail.com>
2023-08-05 16:39:09 +0100

    Extract Category Properties and Parent Properties

--- app/category/[id]/page.jsx
+++ app/category/[id]/page.jsx
@@ -9,7 +9,22 @@ export default function page() {
   const { id } = useParams();
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState({});
-  const [properties, setProperties] = useState([])
+
+  const propertiesToFill = [];
+  if (category) {
+    let catInfo = category;
+    if(catInfo.properties?.length > 0){
+      for(let i = 0; i < catInfo.properties?.length; i++){
+        propertiesToFill.push(catInfo.properties[i]);
+      }
+    }
+    while(catInfo.parent?.properties.length > 0){
+      for(let i = 0; i < catInfo.parent?.properties.length; i++){
+        propertiesToFill.push(catInfo.parent?.properties[i]);
+      }
+      catInfo = catInfo.parent
+    }
+  }
 
   async function fetchCategory() {
     try {
@@ -19,6 +34,13 @@ export default function page() {
       console.log(error);
     }
   }
+
+  async function fetchProducts() {
+    const products = await axios.get(`/api/product/${id}`).then((response) => response.data).catch((error) => error);
+     
+  }
+
+  fetchProducts()
 
   useEffect(
     () => (
@@ -41,14 +63,12 @@ export default function page() {
         <h1 className="font-bold text-3xl">{category && category.name}</h1>
         <h2 className="font-semibold text-xl">Today's Best Deals For You !</h2>
         <div className="mt-5 flex gap-8">
-          {category.parent?.properties.map((index, property) => {
-            return(
-              <select name={property}>
-                {category.parent.properties[property].values.map((value) => {
-                  return(<option value={value}>{value}</option>)
-                })}
-              </select>
-            )
+          {propertiesToFill.map((property, index) => {
+            return(<select name={property.name} id={property.name}>
+              {property.values.map((value, index) => {
+                return(<option value={value}>{value}</option>)
+              })}
+            </select>)
           })}
         </div>
         <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
