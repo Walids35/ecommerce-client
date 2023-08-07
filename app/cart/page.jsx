a2ea3b6a2c461b684f259deccc4176c97041131e
Walid Siala <wsiala4@gmail.com>
2023-08-07 11:07:10 +0100

    Adding Cart Form Component

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -4,6 +4,7 @@ import ProductCart from "@/components/product/ProductCart";
 import Link from "next/link";
 import { TruckIcon, GiftIcon } from "@heroicons/react/24/outline";
 import { useStore } from "@/store/store";
+import CartForm from "@/components/Forms/CartForm";
 
 const CartComponent = () => {
 
@@ -28,6 +29,11 @@ const CartComponent = () => {
     <>
       <div className="px-10 py-10 sm:px-10 md:px-32 lg:px-60">
         <h1 className="font-bold text-3xl">Cart</h1>
+        <div className="mt-8">
+              <Link href="/" className="bg-blue text-white mt-10 px-6 py-3">
+               &lt; Continue Shopping
+              </Link>
+            </div>
         <div className="grid grid-cols-1 xl:grid-cols-3 gap-10">
           <div className="md:col-span-2">
             <div className="mt-5 border w-full border-gray-300">
@@ -43,11 +49,8 @@ const CartComponent = () => {
                 })}
               </div>
             </div>
-            <div className="mt-8">
-              <Link href="/" className="bg-blue text-white mt-10 px-6 py-3">
-                Continue Shopping
-              </Link>
-            </div>
+            <CartForm />
+            
           </div>
           <div>
             <div className="mt-5 border w-full border-gray-300 ">
