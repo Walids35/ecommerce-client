4870d65b80d48b2863b2a634e99956e925c17e0f
Walid Siala <wsiala4@gmail.com>
2023-08-07 15:13:27 +0100

    fix

--- components/product/ProductCart.jsx
+++ components/product/ProductCart.jsx
@@ -3,21 +3,22 @@ import { useStore } from "@/store/store"
 
 const ProductCart = ({product}) => {
 
-  const deleteFromCart = useStore((store) => store.removeFromCart);
+  const deleteFromCart = useStore((store) => store.removeFromCart); 
+  const totalPrice = product.quantity * product.price;
 
   return (
     <>
         <div className="flex p-10 justify-around" key={product._id}>
-            <img src={product.images[0]} alt="" className="w-16 h-16" />
+            <img src="" alt="" className="w-16 h-16" />
             <div>
                 <h1>{product.title}</h1>
                 <p className="font-medium text-purple">{product.price}£</p>
-                <p className="font-normal text-sm hidden md:block">[64b5a93cc77e31b306739963]</p>
+                <p className="font-normal text-sm hidden md:block">[{product._id}]</p>
             </div>
             <div className="text-start font-normal">
-                <p>{product.price}</p>
+                <p>{totalPrice}£</p>
                 <div className="flex">
-                    <p className="py-2 px-5 bg-gray-200">1</p>
+                    <p className="py-2 px-5 bg-gray-200">{product.quantity}</p>
                     <button className="border py-2 px-5 hover:bg-slate-200">+</button>
                     <button className="border py-2 px-5 hover:bg-slate-200">-</button>
                 </div>
