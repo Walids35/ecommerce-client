e28b34acf18ed49cb1bb3ae3dfbe3c7bcab09859
Walid Siala <wsiala4@gmail.com>
2023-08-17 19:55:23 +0100

    Adding Responsive Carousel to Product Page

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,28 +1,46 @@
-"use client"
-import { useParams } from "next/navigation"
-import { useEffect } from "react"
-import axios from "axios"
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ImageCarousel from "@/components/ImageCarousel";
 
 const page = () => {
-  const {id} = useParams()
-  console.log(id)
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
 
-  const fetchProduct = async() => {
-    try{
+  const fetchProduct = async () => {
+    try {
       const response = await axios.get(`/api/cart/${id}`);
-      console.log(response.data)
-    }catch(error){
-      console.log("Error:", error)
+      console.log(response.data[0]);
+      setProduct(response.data[0]);
+    } catch (error) {
+      console.log("Error:", error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchProduct()
-  },[id])
+    fetchProduct();
+  }, [id]);
 
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="px-10 py-10 md:px-28 lg:px-60">
+        {product && (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          {product.images && (
+            <div>
+              <ImageCarousel images={product.images} />
+            </div>
+          )}
+          <div className="py-10">
+            <h1 className="font-bold text-black text-2xl">{product.title}</h1>
+            <p className="text-sm mt-5">{product.description}</p>
+          </div>
+        </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
