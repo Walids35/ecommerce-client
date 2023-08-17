8093f1b91905f4b64d25538e3aac24666e209c13
Walid Siala <wsiala4@gmail.com>
2023-08-17 15:16:21 +0100

    Styling Clear Whislist Button

--- app/wishlist/page.jsx
+++ app/wishlist/page.jsx
@@ -38,6 +38,7 @@ const WishlistComponent = () =>{
         <div className="px-10 py-10 sm:px-10 md:px-32 lg:px-60">
           <div>
             <h1 className="font-bold text-3xl">Your Wishlist</h1>
+            <button className="py-1 px-3 bg-white border border-blue text-blue hover:bg-blue transition-all duration-300 hover:text-white mt-5" onClick={() =>  ClearWishlist()}>Clear Wishlist</button>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mt-5 gap-10">
               {loading ? (
                 Array.from({ length: 9 }, (_, index) => (
@@ -61,7 +62,6 @@ const WishlistComponent = () =>{
                 ))
               )}
             </div>
-            <button className="py-1 px-3 bg-white border border-black text-black mt-3" onClick={() =>  ClearWishlist()}>Clear Wishlist</button>
           </div>
         </div>
         <Footer />
