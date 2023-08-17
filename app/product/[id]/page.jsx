a115cc4ab6e9304eb8e4352d4041d930f169b46a
Walid Siala <wsiala4@gmail.com>
2023-08-17 15:24:37 +0100

    Get ID from URL and Fetch Product

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,4 +1,25 @@
+"use client"
+import { useParams } from "next/navigation"
+import { useEffect } from "react"
+import axios from "axios"
+
 const page = () => {
+  const {id} = useParams()
+  console.log(id)
+
+  const fetchProduct = async() => {
+    try{
+      const response = await axios.get(`/api/cart/${id}`);
+      console.log(response.data)
+    }catch(error){
+      console.log("Error:", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchProduct()
+  },[id])
+
   return (
     <div>page</div>
   )
