cd10dce635af35ab6d144216da1e44db85a990d3
KhalilFH <khalil.fekihhassen@medtech.tn>
2023-09-16 18:47:28 +0100

    SimilarityAPI Fix

--- app/api/similar/route.js
+++ app/api/similar/route.js
@@ -34,50 +34,58 @@ import { NextResponse } from "next/server";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
+function jaccardSimilarity(propertiesA, propertiesB) {
+  const setA = new Set(Object.keys(propertiesA));
+  const setB = new Set(Object.keys(propertiesB));
 
-function cosineSimilarity(propertiesA, propertiesB) {
-  const keys = new Set([...Object.keys(propertiesA), ...Object.keys(propertiesB)]);
-  let dotProduct = 0;
-  let normA = 0;
-  let normB = 0;
+  const intersectionSize = [...setA].filter(item => setB.has(item)).length;
+  const unionSize = setA.size + setB.size - intersectionSize;
 
-  for (const key of keys) {
-    const valueA = propertiesA[key] || 0;
-    const valueB = propertiesB[key] || 0;
-    dotProduct += valueA * valueB;
-    normA += valueA * valueA;
-    normB += valueB * valueB;
-  }
-
-  if (normA === 0 || normB === 0) {
-    return 0; 
-  }
-
-  return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+  return intersectionSize / unionSize;
 }
 
 export async function POST(req) {
   try {
     await mongooseConnect();
-    const _id  = await req.json();
+    const { _id } = await req.json();
     const referenceProduct = await Product.findById(_id);
+
     if (!referenceProduct) {
+      console.log("Reference product not found");
       return NextResponse.json({ error: "Reference product not found" }, { status: 404 });
     }
+
     const referenceProperties = referenceProduct.properties || {};
+
+    console.log("Reference product:", referenceProduct);
+    console.log("Reference properties:", referenceProperties);
+
+    const similarityThreshold = 1;
+
     const allProducts = await Product.find({ _id: { $ne: referenceProduct._id } });
+
     const similarProducts = allProducts
       .map(product => ({
         ...product.toObject(),
-        similarity: cosineSimilarity(referenceProperties, product.properties || {}),
+        similarity: jaccardSimilarity(referenceProperties, product.properties || {}),
       }))
+      .filter(product => {
+        return product.similarity >= similarityThreshold;
+      })
       .sort((a, b) => b.similarity - a.similarity);
 
-    const numberOfSimilarProducts = 4; 
+    console.log("similar product:", similarProducts);
+    
+    const numberOfSimilarProducts = 4;
     const mostSimilarProducts = similarProducts.slice(0, numberOfSimilarProducts);
 
     return NextResponse.json(mostSimilarProducts);
   } catch (error) {
+    console.error("Error:", error);
     return NextResponse.json({ error: error.message });
   }
 }
+
+
+
+
