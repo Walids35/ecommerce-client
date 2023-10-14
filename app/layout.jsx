d5339582ec4658d434216dc7c49168d4325669e1
KhalilFH <khalil.fekihhassen@medtech.tn>
2023-10-14 09:28:01 +0100

    font fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,23 @@
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import OverNav from "@/components/navbar/OverNav";
+import { Poppins } from 'next/font/google'
 
 export const metadata = {
   title: "MY STORE | HOME"
 };
+const poppins =Poppins({
+  weight:'500',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
+      <head>
+      <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet"/>
+      </head>
       <body>
         <OverNav />
         <NavBar />
