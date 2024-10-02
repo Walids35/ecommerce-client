46174fc1cffdca76b1fc9825e83b96a7240d0cf2
Walid Siala <wsiala4@gmail.com>
2024-10-02 21:09:59 +0100

    Fixed footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,46 +1,41 @@
 const Footer = () => {
   return (
     <>
-      <div className="pb-20 px-10 sm:px-20 md:px-32 xl:px-60  bg-neutral-900 pt-10 text-white">
+      <div className="pb-10 px-10 sm:px-20 md:px-32 xl:px-60  bg-neutral-900 pt-10 text-white">
         <div className="w-full h-0.5 bg-slate-300 rounded-full"></div>
         <div className="grid p-10 grid-cols-1 gap-10 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6">
           <div className="col-span-2">
             <img src="/mini-logo-white.svg" alt="" className="w-14 " />
-            <p className="w-72 mt-5 text-sm">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            <p className="w-72 mt-5 text-sm text-justify leading-relaxed">At MYSTORE, we bring you the latest tech innovations to simplify your life. Whether it's computers, smartphones, tablets, or accessories, we carefully select the best products to meet your needs, offering unbeatable prices and a customer service that’s always here to help.</p>
             <p className="mt-5 font-medium">Accepted Payments</p>
             <img src="/payment-methods.svg" className="w-40 mt-2" alt="" />
           </div>
           <div>
-            <h2 className="font-medium">Department</h2>
-            <p className="mt-2">test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
+            <h2 className="font-semibold text-gray-500">Need help?</h2>
+            <div className="flex flex-col gap-2 mt-5 text-sm">
+            <p>Help Center</p>
+            <p>Shipping & Returns</p>
+            <p>FAQ</p>
+            <p>Contact Us</p>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <h2 className="font-semibold text-gray-500">Popular Categories</h2>
+            <div className="flex flex-col gap-2 mt-5 text-sm">
+              <p>Laptops</p>
+              <p>Tablets</p>
+              <p>Smartphones</p>
+              <p>Accessories</p>
+            </div>
           </div>
           <div>
-            <h2 className="font-medium">About Us</h2>
-            <p className="mt-2">test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-          </div>
-          <div>
-            <h2 className="font-medium">Services</h2>
-            <p className="mt-2">test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-          </div>
-          <div>
-            <h2 className="font-medium">Help</h2>
-            <p className="mt-2">test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
+            <h2 className="font-semibold text-gray-500">Join Our Community</h2>
+            <div className="flex flex-col gap-2 mt-5 text-sm">
+              <p>Facebook</p>
+              <p>Twitter</p>
+              <p>Smartphones</p>
+              <p>Instagram</p>
+            </div>
           </div>
         </div>
         <div className="w-full h-0.5 bg-slate-300 rounded-full"></div>
